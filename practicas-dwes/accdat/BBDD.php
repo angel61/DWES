c363d1ebb789a414f5af9f1d8c9ac85962bd411e
Ángel López Palacios <angellopezpalacios61@gmail.com>
2020-10-28 13:21:56 +0100

    acceso mediante db

--- practicas-dwes/accdat/BBDD.php
+++ practicas-dwes/accdat/BBDD.php
@@ -1,11 +1,25 @@
 <?php
 function validarUsuario(string $us,string $pw):bool {
+    require_once("accdat/constantesDB.php");
+
     $validado = false;
     $tipo = "e";
 
-    if ($us === "a" && $pw === "a") $validado = true;
-
+    @$conn=mysqli_connect(host,us,pw,bd);
+    if($conn){
+        $query="select tipo from usuarios where usuario='$us' and clave='$pw'";
+        $rest=mysqli_query($conn,$query);
+        if(mysqli_num_rows($rest)==1){
+            $validado=true;
+            $row=mysqli_fetch_assoc($rest);
+            $tipo=$row['tipo'];
+        }
+        mysqli_close($conn);
+    }
+    
+//     if ($us === "a" && $pw === "a") $validado = true;
+    
     if($validado){$_SESSION['usuario']=$us;$_SESSION['tipo']=$tipo;}
-    else{unset($_SESSION['usuario']);unset($_SESSION['tipo']);}
+    else{unset($_SESSION['usuario']);unset($_SESSION['tipo']);} 
     return $validado;
 }
---
+++ practicas-dwes/accdat/constantesDB.php
@@ -0,0 +1,7 @@
+
+   <?php
+    const host="localhost";
+    const us="root";
+    const pw="";
+    const bd="escuela";
+    const puerto=3306;
