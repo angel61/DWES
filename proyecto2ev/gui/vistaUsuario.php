1abb7ca43b6f5f4422f10737ed1a3a59a8ee1e6b
Angel <angellopezpalacios61@gmail.com>
2021-02-27 23:20:14 +0100

    avance del proyecto

---
+++ proyecto2ev/gui/vistaUsuario.php
@@ -0,0 +1,24 @@
+  <div class="mt-5 d-flex flex-column justify-content-center gap-3 align-items-center ">
+    <div class="h2 mb-4">Información personal</div>
+    <div class="col-10 col-md-6 card">
+      <div class="card-body">
+          <!--img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"-->
+          <h1 class="card-title h3">Información básica</h1>
+          <?php $this->printInfoPersonal();?>
+      </div>
+    </div>
+    <div class="col-10 col-md-6 card">
+      <div class="card-body">
+          <!--img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"-->
+          <h1 class="card-title h3">Direccion de envio</h1>
+          <?php $this->printDireccionEnvio();?>
+      </div>
+    </div>
+    <div class="col-10 col-md-6 card">
+      <div class="card-body">
+          <!--img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"-->
+          <h1 class="card-title h3">Información básica</h1>
+          <?php $this->printInfoPersonal();?>
+      </div>
+    </div>
+  </div>
